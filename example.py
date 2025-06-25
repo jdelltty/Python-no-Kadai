913d606a1985ccb781ef4ca3ca605ac05ba0a9f5
柴浦哲也 <robe@shibauratetsuyanoMacBook-Air.local>
2025-06-25 15:37:30 +0900

    add_exaple.py

---
+++ example.py
@@ -0,0 +1,35 @@
+# 1.hello world
+print("hello world")
+
+# 2.greet関数
+def greet():
+    print("こんにちは")
+    return
+
+greet()
+
+# 3.print_name関数
+def print_name(nama):
+    print("私の名前は" + name + "です")
+    return
+
+name = input("名前を入力してください: ")
+print_name(name)
+
+# 4.get_greet関数
+def get_greet():
+    return "おはようございます"
+
+message = get_greet()
+print(message)
+
+# 5.add関数
+def add(a,b):
+    return a+b
+
+a = input("1つ目の数字を入力してください")
+a = int(a)
+b = input("2つ目の数字を入力してください")
+b = int(b)
+sum = add(a,b)
+print(sum)
